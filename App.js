eda601cf2ef5bff2d77f03ca6f08e8fd370b8495
柳江浩 <39641581+LJHAAAAA@users.noreply.github.com>
2019-11-27 22:07:01 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+// import {BrowserRouter as Router,Route,Redirect} from 'react-router-dom';
+import Topfix from './InitContainer/Topfix';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Topfix/>
+            </div>
+        )
+    }
+}
